0573b956b9fc36731f2be1d44d9bc677a8c009a8
Do Quang Anh <quanganhsk1720@gmail.com>
2026-01-17 18:37:48 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+require("dotenv").config();
+
+const database = require("./config/database");
+const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
+
+database.connect();
+
+const app = express();
+const port = process.env.PORT;
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+
+//routes
+routeAdmin(app);
+route(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
