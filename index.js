a2b01e58a0112680f92ba6319ce49d83f551c0e7
Do Quang Anh <quanganhsk1720@gmail.com>
2026-01-17 19:25:14 +0700

    config route product-admin

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const database = require("./config/database");
 const route = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
+const systemConfig = require("./config/system");
 
 database.connect();
 
@@ -12,6 +13,8 @@ const app = express();
 const port = process.env.PORT;
 app.set("views", "./views");
 app.set("view engine", "pug");
+//App locals
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.use(express.static("public"));
 
