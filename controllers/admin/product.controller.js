c55fd25fcd3dd2469f2fb5be98d8caa61aad571a
Do Quang Anh <quanganhsk1720@gmail.com>
2026-01-18 10:48:53 +0700

    filter by status and search name

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,51 @@
+const Product = require("../../models/product.model");
+
 //GET /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status == req.query.status,
+    );
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status == "");
+    filterStatus[index].class = "active";
+  }
+  let find = {
+    deleted: false,
+  };
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  let keyword = "";
+  if (req.query.keyword) {
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword, "i");
+    find.title = regex;
+  }
+  const products = await Product.find(find);
+  console.log(products);
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
+    products: products,
+    filterStatus: filterStatus,
+    keyword: keyword,
   });
 };
---
+++ public/admin/js/script.js
@@ -0,0 +1,30 @@
+//filter by status
+const buttonStatus = document.querySelectorAll("[button-status]");
+if (buttonStatus.length > 0) {
+  let url = new URL(window.location.href);
+  buttonStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+      window.location.href = url.href;
+    });
+  });
+}
+//search
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+  let url = new URL(window.location.href);
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (e.target.elements.keyword.value) {
+      url.searchParams.set("keyword", e.target.elements.keyword.value);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href;
+  });
+}
